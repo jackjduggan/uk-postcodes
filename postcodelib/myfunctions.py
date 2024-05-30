89336e0e650b6bd47499c9d8ff15313cf4120545
jackjduggan <20094010@mail.wit.ie>
2024-05-30 13:20:01 +0100

    Added setup.py and tidied up code

--- postcodelib/myfunctions.py
+++ postcodelib/myfunctions.py
@@ -5,44 +5,6 @@ Postcodes are:
 - divided into 2 parts separated by space (outward, inward)
 """
 
-def isNumber(c):
-    return c.isdigit()
-    
-
-sample_postcode = "EC1A 1BB"
-sliced_postcode = []
-sliced_postcode[:] = sample_postcode # https://www.geeksforgeeks.org/python-splitting-string-to-list-of-characters/
-
-print(sliced_postcode)
-# returns: ['E', 'C', '1', 'A', ' ', '1', 'B', 'B']
-
-def checkEachChar(list):
-    for c in list:
-        print(f"Checking: {c}")
-        if isNumber(c) == True:
-            print("True")
-        else:
-            print("False")
-    
-checkEachChar(sliced_postcode)
-
-def formatCode(list):
-    output = []
-    for c in list:
-        if isNumber(c) == True:
-            c = "9"
-            output.append(c)
-        elif c == " ":
-            c = " "
-            output.append(c)
-        else:
-            c = "A"
-            output.append(c)
-    return output
-
-output = formatCode(sliced_postcode)
-print(output)
-
 formats = [
     ["A", "A", "9", "A",  " ", "9", "A", "A"],  # WC postcode area; EC1–EC4, NW1W, SE1P, SW1 | EC1A 1BB
     ["A", "9", "A",       " ", "9", "A", "A"],  # E1, N1, W1 | W1A 0AX
@@ -52,13 +14,35 @@ formats = [
     ["A", "A", "9", "9",  " ", "9", "A", "A"]
 ]
 
+def isNumber(c):
+    return c.isdigit()
+    
+def checkEachChar(list):
+    for c in list:
+        print(f"Checking: {c}")
+        if isNumber(c) == True:
+            print("True")
+        else:
+            print("False")
 
-def compareOutputToFormats(code):
+def sliceCode(code):
     sliced_code = []
-    print(sliced_code)
     sliced_code[:] = code
-    print(sliced_code)
-    formatted_code = formatCode(sliced_code)
+    return sliced_code
+
+def formatCode(list):
+    output = []
+    for c in list:
+        if isNumber(c):
+            output.append("9")
+        elif c == " ":
+            output.append(" ")
+        else:
+            output.append("A")
+    return output
+
+def compareOutputToFormats(code) -> str:
+    formatted_code = formatCode(sliceCode(code))
     print(formatted_code)
     for f in formats:
         if all(x == y for x, y in zip(f, formatted_code)): # https://www.geeksforgeeks.org/python-check-if-two-lists-are-identical/
@@ -68,4 +52,4 @@ def compareOutputToFormats(code):
             print("Postcode not valid")
 
 #compareOutputToFormats(sample_postcode)
-compareOutputToFormats("BC4D 5FV")
+compareOutputToFormats("B4D 5FV")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='postcodelib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='UK postcode validator',
+    author='Jack Duggan'
+)
